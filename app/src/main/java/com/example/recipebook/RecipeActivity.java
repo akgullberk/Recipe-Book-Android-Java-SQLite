20554a5e173fbb3ea32f2f62d69c3a59cf6564dd
Berk Akgül <berkak_gul51@hotmail.com>
2024-05-12 19:00:55 +0300

    init

---
+++ app/src/main/java/com/example/recipebook/RecipeActivity.java
@@ -0,0 +1,23 @@
+package com.example.recipebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class RecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+    }
+
+    public void save(View view){
+
+    }
+
+    public void selectImage(View view){
+
+    }
+}
