d15a24667bb249dc9b83e6bb02caf75cbd790046
Berk Akgül <berkak_gul51@hotmail.com>
2024-05-13 17:27:44 +0300

    menu & binding

--- app/src/main/java/com/example/recipebook/MainActivity.java
+++ app/src/main/java/com/example/recipebook/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.recipebook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.recipebook.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.recipe_menu,menu); // menuyu aktiviteye bağladı.
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId()==R.id.add_recipe){
+            Intent intent = new Intent(this, RecipeActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/recipebook/RecipeActivity.java
+++ app/src/main/java/com/example/recipebook/RecipeActivity.java
@@ -5,12 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.recipebook.databinding.ActivityRecipeBinding;
+
 public class RecipeActivity extends AppCompatActivity {
+
+    private ActivityRecipeBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipe);
+        binding=ActivityRecipeBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
     }
 
     public void save(View view){
