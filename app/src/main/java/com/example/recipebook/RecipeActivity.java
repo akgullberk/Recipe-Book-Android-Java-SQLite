a9722f1bb97a77f4a3f409b7da23805f2ac23a47
Berk Akgül <berkak_gul51@hotmail.com>
2024-05-15 14:42:39 +0300

    Permission

--- app/src/main/java/com/example/recipebook/RecipeActivity.java
+++ app/src/main/java/com/example/recipebook/RecipeActivity.java
@@ -1,11 +1,16 @@
 package com.example.recipebook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.recipebook.databinding.ActivityRecipeBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 public class RecipeActivity extends AppCompatActivity {
 
@@ -26,6 +31,22 @@ public class RecipeActivity extends AppCompatActivity {
     }
 
     public void selectImage(View view){
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+
+                Snackbar.make(view,"Permission needed for gallery",Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }).show();
+            }else{
+
+            }
+        }else{
+
+        }
 
     }
 }
