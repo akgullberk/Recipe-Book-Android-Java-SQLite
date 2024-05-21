caf56c89f76da1b4803dba14b224f0be3687dd4b
Berk Akgül <berkak_gul51@hotmail.com>
2024-05-21 20:02:36 +0300

    Pulling Data

--- app/src/main/java/com/example/recipebook/MainActivity.java
+++ app/src/main/java/com/example/recipebook/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,9 +14,12 @@ import android.view.View;
 
 import com.example.recipebook.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    ArrayList<Recipe> recipeArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,38 @@ public class MainActivity extends AppCompatActivity {
         binding=ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        recipeArrayList=new ArrayList<>();
+
+        getData();
+
+    }
+
+    private void getData(){
+
+        try{
+
+            SQLiteDatabase sqLiteDatabase = this.openOrCreateDatabase("Recipes",MODE_PRIVATE,null);
+
+            Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM recipes",null);
+            int nameIx=cursor.getColumnIndex("recipename");
+            int idIx = cursor.getColumnIndex("id");
+
+            while(cursor.moveToNext()){
+                String name = cursor.getString(nameIx);
+                int id = cursor.getInt(idIx);
+                Recipe recipe = new Recipe(name,id);
+                recipeArrayList.add(recipe);
+
+
+            }
+
+            cursor.close();
+        }catch (Exception e){
+            e.printStackTrace();
+
+        }
+
 
     }
 
---
+++ app/src/main/java/com/example/recipebook/Recipe.java
@@ -0,0 +1,13 @@
+package com.example.recipebook;
+
+public class Recipe {
+    String name;
+    int id;
+
+    public Recipe(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+
+}
